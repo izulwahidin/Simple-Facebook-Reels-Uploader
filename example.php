99081a5fad9a70dd304cf9186b7eb5807d09cceb
Izul Wahidin <iam@wahidin.eu.org>
2023-03-05 10:31:45 +0700

    Update example.php

--- example.php
+++ example.php
@@ -2,13 +2,13 @@
 require(__DIR__.'/vendor/autoload.php');
 
 // init Reels API
-$fb_reels = new Wahidin\Fb\Reels(
+$fb_reels = new Wahidin\Facebook\Reels(
     'EAATnSDqIOLMBAOxxxxx' //your facebook page token
 );
 
 try {
     // try to upload
-    echo "Uploading ".basename($config->video_file).PHP_EOL;
+    echo "Uploading local.mp4\n";
     $reel = $fb_reels->upload(
         'local.mp4',
         'my video description'
